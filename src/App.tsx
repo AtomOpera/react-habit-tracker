722d04839b2cdec3215d2692eb6434c904ef64b7
Abri Cabrera-Valdivia <abri.cabreravaldivia@skyscanner.net>
2026-07-17 13:14:21 +0100

    Add normal useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
+import { useState } from "react";
+import type { SubmitEvent } from "react";
 import { Button } from "./Components/Button";
-
+import {startOfWeek, endOfWeek, eachDayOfInterval, format, isFuture, isSameDay, subDays} from "date-fns";
 export default function App() {
+  const [habits, setHabits] = useState<Habit[]>([
+    // { id: "1", name: "Exercise" }
+  ]);
+
+  function addHabit(name: string) {
+    const newHabit = {
+      id: crypto.randomUUID(),
+      name,
+      completions: []
+    };
+    setHabits((prevHabits) => [...prevHabits, newHabit]);
+  }
+
+  function deleteHabit(id: string) {
+    setHabits((prevHabits) => prevHabits.filter((habit) => habit.id !== id));
+  }
+
+  function toggleHabit(id: string, date: Date) {
+    setHabits((curr) =>
+      curr.map((h) => {
+        if (h.id === id) {
+          const isCompleted = h.completions.some((d) => isSameDay(d, date));
+          const newCompletions = isCompleted
+            ? h.completions.filter((d) => !isSameDay(d, date))
+            : [...h.completions, date];
+          return { ...h, completions: newCompletions };
+        }
+        return h;
+      })
+    );
+  }
+
   return (
     <div className="max-w-2xl mx-auto px-4 flex flex-col gap-4">
       <Header />
-      <HabitForm />
-      <HabitList/>
+      <HabitForm addHabit={addHabit} />
+      <HabitList habits={habits} deleteHabit={deleteHabit} toggleHabit={toggleHabit} />
     </div>
   );
 }
@@ -28,61 +62,129 @@ function Header() {
   );
 }
 
-function HabitForm() {
+type HabitFormProps = {
+  addHabit: (name: string) => void;
+};
+
+function HabitForm({ addHabit }: HabitFormProps) {
+  const [name, setName] = useState("");
+
+  function handleSubmit(e: SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if(name.trim() === "") return;
+    addHabit(name);
+    setName("");
+  }
+
   return (
-    <form className="flex gap-2">
+    <form className="flex gap-2" onSubmit={handleSubmit}>
       <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
         type="text"
-        placeholder="New habit"
+        placeholder="New habit..."
         className="flex-1 rounded-lg bg-zinc-800 
         px-4 py-2 outline-none focus-visible:ring-2
         focus-visible:ring-violet-500"
       />
-      <button
+      <Button
         type="submit"
-        className="bg-blue-500 text-white rounded px-3 py-2"
+        className="rounded-lg px-4 py-2 font-medium"
       >
         Add Habit
-      </button>
+      </Button>
     </form>
   );
 }
 
-function HabitList() {
-  const habits = [
-    { id: "1", name: "Exercise" },
-    { id: "2", name: "Read" },
-    { id: "3", name: "Meditate" }
-  ];
+type Habit = {
+  id: string;
+  name: string;
+  completions: Date[]; // Array of dates when the habit was completed
+};
+
+type HabitsListProps = {
+  habits: Habit[];
+  deleteHabit: (id: string) => void;
+  toggleHabit: (id: string, date: Date) => void;
+};
+
+function HabitList( {habits, deleteHabit, toggleHabit}: HabitsListProps) {
+  // const habits = [
+  //   { id: "1", name: "Exercise" },
+  //   { id: "2", name: "Read" },
+  //   { id: "3", name: "Meditate" }
+  // ];
+  if (habits.length === 0) {
+    return (
+      <p className="text-center text-zinc-500 py-12">
+        No habits yet. Add one above to get started!
+      </p>
+    )
+  }
+
   return (
     <ul className="flex flex-col gap-2">
       {habits.map((habit) => (
-        <li key={habit.id} className="flex items-center justify-between bg-zinc-800 p-4 rounded-lg">
-          <span>{habit.name}</span>
-          <input type="checkbox" />
-        </li>
+        <HabitItem key={habit.id} habit={habit} deleteHabit={deleteHabit} toggleHabit={toggleHabit} />
       ))}
     </ul>
   );
 }
 
 type HabitItemProps = {
-  habit: {
-    id: string;
-    name: string;
-  };
+  habit: Habit;
+  deleteHabit: (id: string) => void;
+  toggleHabit: (id: string, date: Date) => void;
 };
 
-function HabitItem({ habit }: HabitItemProps) {
+function HabitItem({ habit, deleteHabit, toggleHabit }: HabitItemProps) {
+  const visibleDates = eachDayOfInterval({
+    start: startOfWeek(new Date(), { weekStartsOn: 1 }),
+    end: endOfWeek(new Date(), { weekStartsOn: 1 })
+  });
+  const streak = getStreak(habit.completions)
+  console.log("streak", streak, habit.completions);
+
   return (
-    <div className="rounded-x1 bg-zinc-800 p-4 flex flex-col gap-3">
+    <div className="rounded-xl bg-zinc-800 p-4 flex flex-col gap-3">
+      {/* header */}
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-3">
           <span className="font-medium">{habit.name}</span>
-          <span className="text-sm text-amber-400">🔥 3</span>
+          {streak !== 0 && (
+            <span className="text-sm text-amber-400">🔥 {streak}</span>
+          )}
         </div>
-        <Button>Delete</Button>
+        <Button variant="ghost-destructive" className="text-sm" onClick={() => deleteHabit(habit.id)}>Delete</Button>
+      </div>
+      {/* footer */}
+      <div className="flex gap-1.5">
+        {visibleDates.map((date) => (
+          <Button 
+            className="flex flex-1 flex-col items-center gap-0.5 rounded-lg text-xs" 
+            key={date.toISOString()} 
+            disabled={isFuture(date)}
+            variant={habit.completions.some(d => isSameDay(d, date)) ? "primary" : "secondary"}
+            onClick={() => toggleHabit(habit.id, date)}
+          >
+            <span className="font-medium">{format(date, "EEE")}</span>
+            <span>{format(date, "d")}</span>
+          </Button>
+        ))}
       </div>
     </div>
   );
+}
+
+function getStreak(completions: Date[]) {
+  let streak = 0
+  let date = new Date()
+
+  while (completions.some(c => isSameDay(c, date))) {
+    streak++
+    date = subDays(date, 1)
+  }
+
+  return streak
 }
--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,14 +1,40 @@
-import type { ReactNode } from "react"
+import type { ComponentProps } from "react"
+import { twMerge } from "tailwind-merge";
 
-export type ButtonProps = {
-    children: ReactNode
+type ButtonProps = {
+  variant?: "primary" | "secondary" | "ghost-destructive";
+} & ComponentProps<"button">;
+
+// type Variant = "primary" | "secondary" | "ghost-destructive";
+
+// ...props is passing props to the button element, 
+// which allows us to use all the props that a button element can have, 
+// like onClick, disabled, etc.
+// It also allows us to use the children prop, which is the content of the button.
+// And it also passed the key prop, which is used to identify the button in a list of buttons (props.key).
+// e.g. <Button onClick={() => console.log("clicked")}>Click me</Button> will be 
+// props.onClick = () => console.log("clicked") and children will be "Click me".
+
+export function Button({variant = "primary", className, ...props}: ButtonProps) {
+    return <button 
+    {...props}
+    className={twMerge(
+      "transition-colors rounded px-2 py-1 disabled:opacity-30 disabled:cursor-not-allowed",
+      getVariantStyles(variant),
+      className
+    )}
+    />
 }
 
-export function Button({children}: ButtonProps) {
-    return <button className="
-    bg-violet-600 
-    hover:bg-violet-500 
-    transition-colors 
-    rounded px-2 py-1 disabled:opacity-30
-    disabled:cursor-not-allowedß">{children}</button>
-}
+function getVariantStyles(variant: ButtonProps["variant"]) { // this could have been its own type, but we can use the ButtonProps type to get the variant type
+  switch (variant) {
+    case "primary":
+      return "bg-violet-600 hover:bg-violet-500";
+    case "secondary":
+      return "bg-zinc-700 hover:bg-zinc-600 text-zinc-400";
+    case "ghost-destructive":
+      return "hover:bg-red-800 text-red-800 hover:text-red-200";
+    default:
+      throw new Error(`Unknown variant: ${variant satisfies never}`);
+  }
+}   
